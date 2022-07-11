2f0d22564f750614552c1c350b8dcb1fdf67385a
germanduy <duyldth2106007@fpt.edu.vn>
2022-07-11 11:01:25 +0700

    aa

---
+++ ConsoleApp3/ConsoleApp3/Product.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Product
+    {
+        private List<Product> list = null;
+
+        public Product()
+        {
+            list = new List<Product>();
+        }
+        public string id { get; set; }
+        public string productname { get; set; }
+        public string price { get; set; }
+
+        public void addpd()
+        {
+            Product add = new Product();
+            Console.WriteLine("Id");
+            add.id = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Product Name");
+            add.productname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Price");
+            add.price = Convert.ToString(Console.ReadLine());
+
+            list.Add(add);
+        }
+        public void displaypd(List<Product> listpd)
+        {
+            Console.WriteLine("{0, -30} {1, -30} {2, -30}",
+                            "Product ID", "Product Name", "Price");
+            if (listpd != null && listpd.Count > 0)
+            {
+                foreach (Product pd in listpd)
+                {
+                    Console.WriteLine("{0, -30} {1, -30} {2, -30}",
+                                      pd.id, pd.productname, pd.price);
+                }
+            }
+            Console.WriteLine();
+        }
+        public List<Product> getProduct()
+        {
+            return list;
+        }
+
+        public Product FindByID(string id)
+        {
+            Product searchResult = null;
+            if (list != null && list.Count > 0)
+            {
+                foreach (Product searchpd in list)
+                {
+                    if (searchpd.id.Equals(id))
+                    {
+                        searchResult = searchpd;
+                    }
+                }
+            }
+            return searchResult;
+        }
+
+        public bool deletebyid(string id)
+        {
+            bool deleteID = false;
+            Product pddelete = FindByID(id);
+            if (pddelete != null)
+            {
+                deleteID = list.Remove(pddelete);
+            }
+            return deleteID;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product aa = new Product();
+            while (true)
+            {
+                Console.WriteLine("\nChoose the function you want");
+                Console.WriteLine("1. Add product records");
+                Console.WriteLine("2. Display product records");
+                Console.WriteLine("3. Delete product by Id");
+                Console.WriteLine("4. Exit");
+                int x = Convert.ToInt32(Console.ReadLine());
+                switch (x)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Add product records");
+                            aa.addproduct();
+                            Console.WriteLine(" successfully");
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            Console.WriteLine("Display product records");
+                            aa.displayproduct(aa.getProduct());
+                            break;
+                        }
+                    case 3:
+                        {
+                            string id;
+                            Console.WriteLine("Delete product by Id");
+                            Console.Write("\nNhap ID: ");
+                            id = Console.ReadLine();
+                            if (aa.deleteproduct(id))
+                            {
+                                Console.WriteLine("successfully", id);
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Exit");
+                            return;
+                        }
+                    default:
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
